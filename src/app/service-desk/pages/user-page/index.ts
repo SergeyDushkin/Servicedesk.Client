df65c828eadde85f0134d828149bc3cd9091b9ef
Sergey Dushkin <leirby@live.ru>
2017-04-01 18:39:58 +0300

    user page

--- src/app/service-desk/pages/user-page/index.ts
+++ src/app/service-desk/pages/user-page/index.ts
@@ -2,37 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subject } from 'rxjs';
+import { BaseTabPage } from "app/service-desk/shared/pages/base-tab-page";
 
 @Component({
   selector: 'app-user-page',
   templateUrl: 'index.html'
 })
-export class UserPage implements OnInit {
+export class UserPage extends BaseTabPage implements OnInit {
 
-  id: string;
+  tabs = [
+    { id: 0, type: 'app-user-detail', title: 'Главная' },
+  ];
 
   constructor(private route: ActivatedRoute, private location: Location, private router: Router) { 
+    super();
   }
 
   ngOnInit() {
     this.route.params.subscribe(data => {
       this.id = data['id'];
+      this.init();
     });
-  }
-
-  handleLoaded(data) {
-    console.log('handleLoaded ' + data);
-  }
-
-  handleCanceled($event, link) {
-    console.log('handleLoaded ' + $event);
-  }
-
-  handleUpdated($event, link) {
-    console.log('handleLoaded ' + $event);
-  }
-  handleDeleted($event, link) {
-    console.log('handleLoaded ' + $event);
   }
 
 }
